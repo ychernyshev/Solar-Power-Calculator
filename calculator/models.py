f524c097d76a879fd4d14bc1dac0e1d036baefb0
Eugene <eugene_chernyshev@yahoo.com>
2025-11-09 15:06:35 +0200

    Calculation of power and cost
    Added an automation for calculating the generated solar power in watts and calculating the cost of  generated solar power

--- calculator/models.py
+++ calculator/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.expressions import result
 
 
 # Create your models here.
@@ -19,6 +20,33 @@ class DataEntryLineModel(models.Model):
     afternoon_data_price = models.FloatField(verbose_name='Вартість використаної енергії на день')
     evening_data_charge = models.IntegerField(verbose_name='Вечірній рівень заряду')
     evening_data_price = models.FloatField(verbose_name='Вартість використаної енергії на вечір')
-    full_day_power = models.FloatField(verbose_name='Вироблена потужність за день')
-    full_day_cost = models.FloatField(verbose_name='Вартість виробленої енергії за день')
-    power_tariff = models.FloatField(verbose_name='Вартість за Кв')
+    full_day_power = models.FloatField(blank=True, verbose_name='Вироблена потужність за день')
+    full_day_cost = models.FloatField(blank=True, verbose_name='Вартість виробленої енергії за день')
+    power_tariff = models.FloatField(verbose_name='Вартість за Кв', default='4.32')
+
+    def _calculate_full_day_power(self):
+        try:
+            return (self.afternoon_data_charge - self.evening_data_charge) * 20.48 + (
+                        (self.afternoon_data_price - self.evening_data_price) / 43.2) * 100
+        except(TypeError, ZeroDivisionError):
+            return 0.0
+
+
+    def _calculate_full_day_cost(self):
+        try:
+            return (((self.afternoon_data_charge - self.evening_data_charge) * 20.48 + (
+                        (self.afternoon_data_price - self.evening_data_price) / 43.2) * 100) / 1000) * 4.32
+        except(TypeError, ZeroDivisionError):
+            return 0.0
+
+
+    def save(self, *args, **kwargs):
+        calculated_power = self._calculate_full_day_power()
+        self.full_day_power = calculated_power
+
+        calculated_cost = self._calculate_full_day_cost()
+        self.full_day_cost = calculated_cost
+
+        super().save(*args, **kwargs)
+
+
