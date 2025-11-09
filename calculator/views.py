15132938a4b1c645c6af67d9966811124c4bc1f9
Eugene <eugene_chernyshev@yahoo.com>
2025-11-09 08:10:50 +0200

    Add initial 'calculator' app structure
    Added the 'calculator' Django app to manage the daily solar energy system generation
    
    Configured:
    - App registered in INSTALLED_APPS.
    - Base urls.py created and included in the project's root urls.
    - Initial index view and template path added.

---
+++ calculator/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.template.context_processors import request
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'calculator/index.html')
