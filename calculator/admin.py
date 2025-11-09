1836eb1442d7cdac51b97e714523170a3c3e2fa8
Eugene <eugene_chernyshev@yahoo.com>
2025-11-09 13:00:53 +0200

    Update admin.py and model.py
    Updates:
    - Entry line view on the Django admin panel has been customized
    - To all entry line fields has been added verbose_name

--- calculator/admin.py
+++ calculator/admin.py
@@ -1,3 +1,74 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
+from .models import DataEntryLineModel
+
 
 # Register your models here.
+@admin.register(DataEntryLineModel)
+class DataEntryLineAdmin(admin.ModelAdmin):
+    list_display = ['date', 'display_power', 'weather',
+                    'display_morning_charge', 'display_morning_price',
+                    'display_afternoon_charge', 'display_afternoon_price',
+                    'display_evening_charge', 'display_evening_price',
+                    'display_full_day_power', 'display_full_day_cost', 'display_power_tariff']
+
+
+    def display_power(self,obj):
+        return format_html('{}Вт', obj.power)
+
+    display_power.short_description = 'Потужність системи'
+
+
+    def display_morning_charge(self, obj):
+        return format_html('{}%', obj.morning_data_charge)
+
+    display_morning_charge.short_description = 'Ранковий рівень заряду'
+
+
+    def display_afternoon_charge(self, obj):
+        return format_html('{}%', obj.afternoon_data_charge)
+
+    display_afternoon_charge.short_description = 'Денний рівень заряду'
+
+
+    def display_evening_charge(self, obj):
+        return format_html('{}%', obj.evening_data_charge)
+
+    display_evening_charge.short_description = 'Вечірній рівень заряду'
+
+
+    def display_morning_price(self, obj):
+        return format_html('{}₴', obj.morning_data_price)
+
+    display_morning_price.short_description = 'Вартість використаної енергії на ранок'
+
+
+    def display_afternoon_price(self, obj):
+        return format_html('{}₴', obj.afternoon_data_price)
+
+    display_afternoon_price.short_description = 'Вартість використаної енергії за день'
+
+
+    def display_evening_price(self, obj):
+        return format_html('{}₴', obj.evening_data_price)
+
+    display_evening_price.short_description = 'Вартість використаної енергії на вечір'
+
+
+    def display_full_day_cost(self, obj):
+        return format_html('{}₴', obj.full_day_cost)
+
+    display_full_day_cost.short_description = 'Вартість виробленої енергії за день'
+
+
+    def display_full_day_power(self,obj):
+        return format_html('{}Вт', obj.full_day_power)
+
+    display_full_day_power.short_description = 'Вироблена потужність за день'
+
+
+    def display_power_tariff(self, obj):
+        return format_html('{}₴', obj.power_tariff)
+
+    display_power_tariff.short_description = 'Вартість за Кв'
--- calculator/models.py
+++ calculator/models.py
@@ -10,16 +10,15 @@ class DataEntryLineModel(models.Model):
         ('800', '800'),
     ]
 
-    date = models.DateField(default=False)
-    power = models.CharField(choices=POWER, max_length=3, default='600')
-    weather = models.CharField(max_length=20)
-    morning_data_charge = models.IntegerField()
-    morning_data_price = models.FloatField()
-    afternoon_data_charge = models.IntegerField()
-    afternoon_data_price = models.FloatField()
-    evening_data_charge = models.IntegerField()
-    evening_data_price = models.FloatField()
-    full_day_power = models.FloatField()
-    full_day_cost = models.FloatField()
-    power_tariff = models.FloatField()
-    
+    date = models.DateField(default=False, verbose_name='Дата')
+    power = models.CharField(choices=POWER, max_length=3, default='600', verbose_name='Потужність системи')
+    weather = models.CharField(max_length=20, verbose_name='Погода')
+    morning_data_charge = models.IntegerField(verbose_name='Ранковий рівень заряду')
+    morning_data_price = models.FloatField(verbose_name='Вартість використаної енергії на ранок')
+    afternoon_data_charge = models.IntegerField(verbose_name='Денний рівень заряду')
+    afternoon_data_price = models.FloatField(verbose_name='Вартість використаної енергії на день')
+    evening_data_charge = models.IntegerField(verbose_name='Вечірній рівень заряду')
+    evening_data_price = models.FloatField(verbose_name='Вартість використаної енергії на вечір')
+    full_day_power = models.FloatField(verbose_name='Вироблена потужність за день')
+    full_day_cost = models.FloatField(verbose_name='Вартість виробленої енергії за день')
+    power_tariff = models.FloatField(verbose_name='Вартість за Кв')
