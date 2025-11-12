ac96493177f95662eddb52c6350fc5c8b0c175d2
Eugene <eugene_chernyshev@yahoo.com>
2025-11-12 19:23:11 +0200

    Topnav active links
    Interactions have been added for top nav menu links. Link color highlights when the page of this link is active

--- calculator/urls.py
+++ calculator/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='calculator'),
+    path('', index, name='home'),
+    path('dashboard/', index, name='dashboard'),
+    path('add_entry/', add_entry, name='add_entry'),
 ]
--- calculator/views.py
+++ calculator/views.py
@@ -5,3 +5,7 @@ from django.template.context_processors import request
 # Create your views here.
 def index(request):
     return render(request, 'calculator/index.html')
+
+
+def add_entry(request):
+    return render(request, 'calculator/add_entry.html')
