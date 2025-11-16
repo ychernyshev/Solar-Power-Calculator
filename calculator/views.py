9404a8264c344936ac0aa1fb877b9edddb9aa87f
Eugene <eugene_chernyshev@yahoo.com>
2025-11-16 20:02:55 +0200

    Entries data on the screen
    The entry data has been shown on the screen in the Dashboard page

--- calculator/views.py
+++ calculator/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
+
 from calculator.services.handle_entry_form import handle_entry_form
+from calculator.models import DataEntryLineModel
 
 
 # Create your views here.
 def index(request):
+    entries = DataEntryLineModel.objects.all()
+
     form, response = handle_entry_form(request)
     if response:
         return response
 
     context = {
+        'entries': entries,
         'form': form,
     }
 
