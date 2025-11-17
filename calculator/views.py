4504f81b0a7b31ff7fa98422e99075ff1dedec44
Eugene <eugene_chernyshev@yahoo.com>
2025-11-17 12:47:07 +0200

    Pagination
    Dashboard updated:
    - The entries table paginnation has been added

--- calculator/views.py
+++ calculator/views.py
@@ -1,3 +1,5 @@
+from django.contrib.admin.templatetags.admin_list import pagination
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
 from calculator.services.handle_entry_form import handle_entry_form
@@ -13,17 +15,24 @@ def index(request):
     if response:
         return response
 
+    # charts
     labels = [entry.date.strftime("%d.%m.%Y") for entry in entries]
     power_values = [entry.full_day_power for entry in entries]
     cost_values = [entry.full_day_cost for entry in entries]
+
+    # pagination
+    entries_paginator = Paginator(entries, 50)
+    entries_number = request.GET.get('page')
+    entries_numbers = entries_paginator.get_page(entries_number)
 
     context = {
         'entries': entries,
         'total': total,
         'form': form,
-        "labels": labels,
-        "power_values": power_values,
-        "cost_values": cost_values,
+        'labels': labels,
+        'power_values': power_values,
+        'cost_values': cost_values,
+        'entries_numbers': entries_numbers
     }
 
     return render(request, 'calculator/index.html', context=context)
