f8e4ca619643b2327bbcc9ee7460c333547c7698
Eugene <eugene_chernyshev@yahoo.com>
2025-11-19 19:48:50 +0200

    Update models.py
    Small changes and fixes in the app's mathematics

--- calculator/models.py
+++ calculator/models.py
@@ -71,22 +71,26 @@ class DataEntryLineModel(models.Model):
                 #     if self.evening_data_charge - self.morning_data_charge < 10:
                 #         return 1.99584
                 if self.afternoon_data_charge > self.evening_data_charge:
-                    cost_error = (self.afternoon_data_charge - self.evening_data_charge) * 20.48
-                    if cost_error < 100:
-                        return (((((
-                                           self.evening_data_price - self.afternoon_data_price) * 100) / 43.2) * 100) / 1000) * 4.32 - (
-                                (cost_error / 1000) * 4.32)
-                    if cost_error >= 100:
-                        return (((((
-                                           self.evening_data_price - self.afternoon_data_price) * 100) / 43.2) * 100) / 1000) * 4.32 - (
-                                (cost_error / 100) * 4.32)
-                    return 0
+                    if 10 < self.morning_data_charge - self.evening_data_charge < 30:
+                        return 4.32 / 5
+                    if self.morning_data_charge - self.evening_data_charge < 10:
+                        return 4.32 / 10
+                    # cost_error = (self.afternoon_data_charge - self.evening_data_charge) * 20.48
+                    # if cost_error < 100:
+                    #     return (((((
+                    #                        self.evening_data_price - self.afternoon_data_price) * 100) / 43.2) * 100) / 1000) * 4.32 - (
+                    #             (cost_error / 1000) * 4.32)
+                    # if cost_error >= 100:
+                    #     return (((((
+                    #                        self.evening_data_price - self.afternoon_data_price) * 100) / 43.2) * 100) / 1000) * 4.32 - (
+                    #             (cost_error / 100) * 4.32)
+                    # return 0
             if self.afternoon_data_price == 0:
                 if self.morning_data_charge > self.evening_data_charge:
                     if 10 < self.morning_data_charge - self.evening_data_charge < 30:
-                        return 0.999792
+                        return 0.43
                     if self.morning_data_charge - self.evening_data_charge < 10:
-                        return 1.99584
+                        return 0.86
                 if self.morning_data_charge < self.evening_data_charge & self.morning_data_charge != 0:
                     # return ((((self.evening_data_price - (self.morning_data_price + 0.60)) / 43.2) * 100) / 1000) * 4.32
                     return (((self.evening_data_charge - (self.morning_data_charge - 6)) * 20.48 + (
